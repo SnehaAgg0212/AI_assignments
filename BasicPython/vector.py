2c60a0705a278e84e2b2521c16d65ec53e43a359
SnehaAgg0212 <aggarwalsneha1999@gmail.com>
2021-01-15 11:32:43 +0530

    changed length function in vector.py, updated notebook with the outputs

--- BasicPython/vector.py
+++ BasicPython/vector.py
@@ -1,5 +1,5 @@
-#Create a suitable class in python to represent the mathematical concept of '1-D vector'
-#(use list data structure to represent 1-D vector). 
+#Create a suitable class in python to represent the mathematical concept of 'vector'
+#(use list data structure to represent vector). 
 # Create appropriate member variables and member functions of this class to perform operations:
 #  Length of vector, Cosine similarity between two vectors, Euclidean distance between two vectors. 
 
@@ -9,7 +9,11 @@ class Vect:
             self.lt = l 
 
     def length(self):
-        return len(self.lt)
+        s = 0
+        for i in range(len(self.lt)):
+            s = s + (self.lt[i] ** 2)
+
+        return math.sqrt(s)
 
     def compute_cosine(self, l):
         sumxx, sumxy, sumyy = 0, 0, 0
@@ -32,8 +36,15 @@ class Vect:
         return math.sqrt(sumxy)
 
 
-v1 = Vect([1,2,3,4])
-v2 = Vect([3,4, 8, 10])
-print (v1.length())
-print (v1.compute_cosine(v2))
-print (v2.EuclideanDist(v1))
+print ("Enter the dimensions for the first vector")
+l1 = [int(x) for x in input().split()]
+print ("Enter the dimensions for the second vector")
+l2 = [int(x) for x in input().split()]
+if len(l1) != len(l2):
+    print ("There is a mismatch in the dimensions of the two vectors, please try again")
+else:
+    v1 = Vect(l1)
+    v2 = Vect(l2)
+    print ("Length of the vector: ",v1.length())
+    print ("Computing the cosine: ",v1.compute_cosine(v2))
+    print ("Computing the Euclidean distance: ",v2.EuclideanDist(v1))
